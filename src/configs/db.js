94e8300ad236c28d50d7fed762e747fcc404471b
Rutuja Patil <rspatil3562@gmail.com>
2022-10-27 11:35:26 +0530

    update

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
 const connect=()=>{return mongoose.connect(
-  "mongodb+srv://Rutu:Rutuatlas3562@cluster0.4soie.mongodb.net/registerapi?retryWrites=true&w=majority"
+  "mongodb+srv://Rutu:Rutuatlas3562@cluster0.4soie.mongodb.net/register?retryWrites=true&w=majority"
   // "mongodb+srv://rutuja:Rutuja_3562@cluster0.orwkt.mongodb.net/test"
 //  "mongodb://localhost:27017/register-api"
 //  "mongodb://127.0.0.1:27017/registerapi"
